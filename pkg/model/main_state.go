2f3f5ccc23dd97842bad8caa3e5ae7a8ffc5f4eb
Korppi <3602291+Korppi@users.noreply.github.com>
2024-01-28 18:33:55 +0200

    testing Every cmd and borders

--- pkg/model/main_state.go
+++ pkg/model/main_state.go
@@ -1,23 +1,42 @@
 package model
 
 import (
+	"fmt"
+	"time"
+
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type MainState struct {
 	Text  string
 	state Substate
+	count int
 }
 
 func NewMainState() *MainState {
 	return &MainState{Text: "Main :)", state: &MenuState{Text: "Menu :O"}}
 }
 
+type TickMsg time.Time
+
+// Send a message every second.
+func tickEvery() tea.Cmd {
+	return tea.Every(time.Second, func(t time.Time) tea.Msg {
+		return TickMsg(t)
+	})
+}
+
 func (m MainState) Init() tea.Cmd {
-	return tea.SetWindowTitle("Hangman")
+	return tea.Batch(tea.SetWindowTitle("Hangman"), tickEvery())
 }
 
 func (m MainState) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg.(type) {
+	case TickMsg:
+		m.count++
+		return m, tickEvery()
+	}
 	stateExitCode := m.state.Update(msg)
 	if stateExitCode == Quit {
 		return m, tea.Quit
@@ -30,8 +49,12 @@ func (m MainState) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m MainState) View() string {
-	text := m.Text
+	text := m.Text + fmt.Sprint(m.count)
 	text += "\n"
 	text += m.state.View()
-	return text
+	style := lipgloss.NewStyle().
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("228")).
+		BorderBackground(lipgloss.Color("63"))
+	return style.Render(text)
 }
